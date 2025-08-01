95c02d10b71551dd9fdadf1bac34a86775d6d54a
guimendes08 <gabrielmendesaraujo107@gmail.com>
2025-08-01 08:35:05 -0300

    Update quest2.c

--- quest2.c
+++ quest2.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 
+int main() {
+    printf("Numero\tQuadrado\tCubo\n"); 
 
-int main(){
-    for (int n = 0; n <= 10; n++){
-        printf("Numero \t Quadrado \t Cubo \n %d \t %d \t  \t %d \n", n, n * n, n * n * n);
+    for (int n = 0; n <= 10; n++) {
+        printf("%d\t%d\t\t%d\n", n, n * n, n * n * n);
     }
+
+    return 0;
 }
